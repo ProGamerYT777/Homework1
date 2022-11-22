554c293219e81dd407ec43647cd6602e1e30df0b
Мирослав <mir.ram05@yandex.ru>
2022-11-22 18:33:17 +0300

    Made HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Задача 1
+        // Задача 1.
         System.out.println("Задача 1");
         int clientOS = 0;
         if (clientOS == 0) {
