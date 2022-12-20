de08b518878eb420c1fca8b8b6a1be2045585e56
Мирослав <mir.ram05@yandex.ru>
2022-12-20 15:10:36 +0300

    Made HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+        // Задача 1
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+        // Задача 2
+        System.out.println("Задача 2");
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+                if (arr[i] > max) {
+                    max = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей.");
+        // Задача 3
+        System.out.println("Задача 3");
+        double average = 0;
+        double sumAmount = 0;
+            for (int i = 0; i < arr.length; i++) {
+                sumAmount += arr[i];
+            }
+            average = sumAmount / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей.");
+        // Задача 4
+        System.out.println("Задача 4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            int tmp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = (char) tmp;
+        }
+        System.out.println(reverseFullName);
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
