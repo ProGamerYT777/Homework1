342e2432d5f7e48c2423a7f45ed650c891e6554a
Мирослав <mir.ram05@yandex.ru>
2022-12-21 17:18:33 +0300

    Made HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+        // Задача 1
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника — " + fullName);
+        // Задача 2
+        System.out.println("Задача 2");
+        fullName = fullName.replace("Ivanov Ivan Ivanovich", "IVANOV IVAN IVANOVICH");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        // Задача 3
+        System.out.println("Задача 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника —  " + fullName1);
     }
 }
