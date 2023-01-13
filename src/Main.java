14f44268f8fe4836414cf87493e381ef4d26bd0b
Мирослав <mir.ram05@yandex.ru>
2023-01-13 22:41:54 +0300

    Made first course

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import repository.DepartmentData;
+import repository.EmployeeData;
+import service.UiService;
+import service.impl.UiServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет, мир!");
+        DepartmentData.getInstance();
+        EmployeeData.getInstance();
+        UiService uiService = new UiServiceImpl();
+        uiService.getStartAppPage();
     }
 }
